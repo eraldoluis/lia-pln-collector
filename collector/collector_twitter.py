811cb9645ce9c0a0442c01ed481249358717c295
Eraldo R. Fernandes <eraldo@facom.ufms.br>
2017-02-20 16:30:41 -0400

    Melhorei tratamento de exceções. O script rodará mesmo depois de quedas de conexão ou exceções do Tweepy.

--- collector/collector_twitter.py
+++ collector/collector_twitter.py
@@ -3,7 +3,11 @@
 
 import sys
 import time
+import traceback
 from datetime import datetime
+from dateutil.parser import parse
+from dateutil import tz
+
 import json
 from tweepy import Stream
 from tweepy import OAuthHandler
@@ -14,37 +18,72 @@ import logging
 import logging.config
 
 
-class Listener(StreamListener):
-    def __init__(self, terms=None):
-        self.es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-        self.start = datetime.now()
+class ResilientListener(StreamListener):
+    def __init__(self, terms, es, auth):
+        super(ResilientListener, self).__init__()
         self.terms = terms
+        self.es = es
+        self.auth = auth
         self.count = 0
+        self.startTime = None
+        self.sleepTime = None
+
+    def start(self):
+        # Start time (this can be used as an ID).
+        self.startTime = datetime.now(tz.tzlocal())
+
+        logging.info("Start: " + str(self.startTime))
+
+        # Create the stream object.
+        twitterStream = Stream(self.auth, self)
+
+        # When an error occurs, sleep for 1 second and try again. If the connection fails, double this time.
+        # If the connection goes through and some tweets are sent, restart this time interval.
+        self.sleepTime = None
+
+        while True:
+            try:
+                # Start tracking.
+                twitterStream.filter(track=self.terms, languages=['pt'])
+            except Exception as e:
+                logging.error(e)
+                print traceback.format_exc()
+
+            if self.sleepTime is None:
+                self.sleepTime = 1
+            else:
+                self.sleepTime *= 2
+
+            # If disconnected, sleep for a while.
+            logging.info('Sleeping for %d seconds...' % self.sleepTime)
+            time.sleep(self.sleepTime)
 
     def on_data(self, data):
         try:
             # Parse JSON data.
             tweet = json.loads(data)
 
+            # print json.dumps(tweet, indent=2)
+
             # Filter some fields of interest.
             user = tweet.get('user', {})
             tweet = {
                 'id': tweet.get('id'),
                 'id_str': tweet.get('id_str'),
-                'user_id': user.get('id'),
-                'user_id_str': user.get('id_str'),
-                'user_screen_name': user.get('screen_name'),
-                'user_name': user.get('name'),
-                'user_location': user.get('location'),
-                'user_created_at': user.get('created_at'),
-                'user_verified': user.get('verified'),
                 'text': tweet.get('text'),
-                'created_at': tweet.get('created_at'),
+                'created_at': parse(tweet.get('created_at')) if 'created_at' in tweet else None,
                 'coordinates': tweet.get('coordinates'),
-                'lang': tweet.get('lang')
+                'lang': tweet.get('lang'),
+                'user': {
+                    'id': user.get('id'),
+                    'id_str': user.get('id_str'),
+                    'screen_name': user.get('screen_name'),
+                    'name': user.get('name'),
+                    'location': user.get('location'),
+                    'created_at': parse(user.get('created_at')) if 'created_at' in user else None,
+                    'verified': user.get('verified')
+                }
             }
-
-            # print json.dumps(tweet, indent=2)
 
             self.count += 1
             if (self.count % 100) == 0:
@@ -54,24 +93,34 @@ class Listener(StreamListener):
             self.es.index(index="ctrls",
                           doc_type="twitter",
                           body={
-                              "start": self.start,
+                              "start": self.startTime,
                               "terms": self.terms,
                               "count": self.count,
                               "tweet": tweet
                           })
 
+            # Reset sleep time due to error.
+            self.sleepTime = None
+
             return True
 
         except Exception as e:
             logging.error(e)
+            print traceback.format_exc()
             return True
 
     def on_error(self, status):
         logging.error(status)
-        return True
+        return False
 
 
 def decodeUtf8(txt):
+    """
+    Make sure the given string is encoded as UTF8.
+
+    :param txt: input string
+    :return: input string encoded as UTF8
+    """
     if type(txt) != unicode:
         txt = txt.decode('utf-8')
     return txt
@@ -104,6 +153,7 @@ if __name__ == "__main__":
     access_token_key = apiConfig['access_token_key']
     access_token_secret = apiConfig['access_token_secret']
 
+    # Make sure input terms are encoded as UTF8.
     keyword_list = [decodeUtf8(s) for s in sys.argv[1:]]
 
     logging.info("Search terms: " + str(keyword_list))
@@ -112,14 +162,8 @@ if __name__ == "__main__":
     auth = OAuthHandler(ckey, consumer_secret)
     auth.set_access_token(access_token_key, access_token_secret)
 
-    try:
-        # Create the stream object.
-        twitterStream = Stream(auth, Listener(terms=keyword_list))
-
-        while True:
-            # Start tracking.
-            twitterStream.filter(track=keyword_list, languages=['pt'])
-            # If disconnected, sleep for 5 minutes and try again.
-            time.sleep(5 * 60)
-    except Exception as e:
-        logging.error(e)
+    # Create the stream object.
+    listener = ResilientListener(terms=keyword_list,
+                                 es=Elasticsearch([{'host': 'localhost', 'port': 9200}]),
+                                 auth=auth)
+    listener.start()
